6a709387d5a886067439d6fc818e0f0e563b7ad5
Chase Anthony Rogers <chase@bellwethermade.com>
2016-07-04 11:55:09 -0500

    refactor fib function

--- lib/fibonacci_elixir.ex
+++ lib/fibonacci_elixir.ex
@@ -1,8 +1,6 @@
 defmodule FibonacciElixir do
 
-  def fibonacci(number) when number <= 1 do
-    number
-  end
+  def fibonacci(number) when number <= 1, do: number
 
   def fibonacci(number), do: fibonacci(number - 1) + fibonacci(number - 2)
 
